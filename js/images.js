18456100cf733d76a2df8b7687f082cd02d2c3c4
Joshua Ferrara <scjosh2@gmail.com>
2019-04-24 21:34:26 -0700

    eslint fixes

--- js/images.js
+++ js/images.js
@@ -1,10 +1,10 @@
 $(document).ready(() => {
-    const imageList = document.querySelector('#images');
+  const imageList = document.querySelector('#images');
 
-    // Load posts
-    $.getJSON('http://localhost:3000/images', (images) => {
-        images.forEach((image) => {
-        $(imageList).append(`<li><img class="pic" src="${image.src}" ><p class="caption">  ${image.caption}</p></li>`);
-      });
+  // Load posts
+  $.getJSON('http://localhost:3000/images', (images) => {
+    images.forEach((image) => {
+      $(imageList).append(`<li><img class="pic" src="${image.src}" ><p class="caption">  ${image.caption}</p></li>`);
     });
+  });
 });
